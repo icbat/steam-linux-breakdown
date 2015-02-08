91b01f9a4195018a09b4257a6df29bbb2af93fb5
icbat <cobb.evan@gmail.com>
2015-02-07 19:47:22 -0600

    pulls back some data about the game

--- steamintegration.py
+++ steamintegration.py
@@ -15,8 +15,7 @@ class Steam:
 		raw_games = libraryJson["response"]["games"]
 		games = []
 		for raw_game in raw_games:
-			game = Game(raw_game["appid"])
-			games.append(game)
+			games.append(raw_game["appid"])
 		return games
 
 	def __read_secret_key(self):
@@ -28,10 +27,17 @@ class Steam:
 		return json.loads(response)
 
 class Game:
-	__id = ""
+	__id = "DEFAULT"
+	name = "DEFAULT"	
+	is_linux = False
 
 	def __init__(self, id):
-		self.__id = id
+		self.__id = id		
+		page_data = self.__get_app_html(id)
+
+	def __get_app_html(self, id):
+		url = "https://store.steampowered.com/app/" + str(id) + "/"
+		return urllib2.urlopen(url).read()
 
 	def __str__(self):
 		return self.__id
@@ -45,7 +51,10 @@ class SteamIntegrationTest(unittest.TestCase):
 		steam = Steam()
 		library = steam.get_library(test_id)
 		self.failIf(len(library) == 0)
-		return
+
+	def test_game_populates(self):
+		happy_game = Game(211820)
+		self.assertTrue(happy_game.is_linux, "Game should be linux-compat but is marked not")
 
 if __name__ == '__main__':
 	unittest.main()
