7992cf66d7e77889e212e9ee751e8002fb4fb983
icbat <cobb.evan@gmail.com>
2015-02-07 19:06:57 -0600

    I think it makes more sense for this to live here

--- server.py
+++ server.py
@@ -2,14 +2,13 @@ from flask import Flask
 from steamintegration import Steam
 app = Flask(__name__)
 
-steam = Steam()
-
 @app.route('/')
 def landing_page():
 	return 'The home page'
 
 @app.route('/<username>')
-def get_breakdown(username):	
+def get_breakdown(username):
+	steam = Steam()
 	return steam.get_library(username)
 
 if __name__ == '__main__':
