0d3042b6b172ced8f28a5ea009e6a3f53c018433
icbat <cobb.evan@gmail.com>
2015-02-07 18:36:06 -0600

    small customizations

--- server.py
+++ server.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def landing_page():
+    return 'The home page'
 
 if __name__ == '__main__':
     app.run()
