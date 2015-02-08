333af8024ca4b8edf04a192d68b9094731a06113
icbat <cobb.evan@gmail.com>
2015-02-07 18:37:57 -0600

    endpoints exist

--- server.py
+++ server.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def landing_page():
-    return 'The home page'
+	return 'The home page'
+
+@app.route('/<username>')
+def get_breakdown(username):
+	return 'hello' + username
 
 if __name__ == '__main__':
-    app.run()
+	app.debug = True
+	app.run()
