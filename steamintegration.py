8fd1ef9c68e9b9f4cdef5ee0cba0abb85a10230d
icbat <cobb.evan@gmail.com>
2015-02-07 19:09:18 -0600

    pulling sensitive data inside the class

--- steamintegration.py
+++ steamintegration.py
@@ -1,14 +1,17 @@
-def read_secret_key():
-	with open ("secret/steam-api-key.secret") as keyfile:
-		content = keyfile.readline()
-		return content
-
-api_key = read_secret_key()
-
 class Steam:
+	__api_key = ""
+
+	def __init__(self):
+		self.__api_key = self.__read_secret_key()
+
 	def get_library(self, user_id):
 		endpoint = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?"
-		endpoint += "key=" + api_key
+		endpoint += "key=" + self.__api_key
 		endpoint += "&steamid=" + user_id
 		endpoint += "&format=json"
 		return endpoint
+
+	def __read_secret_key(self):
+		with open ("secret/steam-api-key.secret") as keyfile:
+			content = keyfile.readline()
+			return content
