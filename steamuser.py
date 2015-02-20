2bd20a53fd1bc6e86ab1a16927ed24e5ae773b64
icbat <cobb.evan@gmail.com>
2015-02-20 13:06:01 -0600

    changing expectation, writing failing tests

--- steamuser.py
+++ steamuser.py
@@ -4,8 +4,9 @@ class User:
 	name = "DEFAULT NAME"
 
 	def __init__(self, user_input):
-		self.id = self.__get_steam_id(user_input)
-		self.name = self.__get_steam_name(self.id)
+		profile_text = self.__get_steam_profile_page(user_input)
+		self.id = self.__get_steam_id(profile_text)
+		self.name = self.__get_steam_name(profile_text)
 
 	def get_library(self, api_key):
 		endpoint = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?"
@@ -20,11 +21,15 @@ class User:
 			appids.append(game_json["appid"])
 		return appids
 
-	def __get_steam_id(self, user_input):
+	def __get_steam_profile_page(self, user_input):
 		return user_input
 
-	def __get_steam_name(self, id):
-		return id
+	def __get_steam_id(self, raw_html):
+		
+		return raw_html
+
+	def __get_steam_name(self, raw_html):
+		return raw_html
 
 	def __get_json(self, endpoint):
 		response = requests.get(endpoint).content
--- test_user.py
+++ test_user.py
@@ -6,12 +6,23 @@ class UserTests(unittest.TestCase):
 	# can run locally though!
 	def skip_get_library_happy(self):
 		test_id = "76561198036780759"
-		steam = User()
+		steam = User(test_id)
 		key_location = "secret/steam-api-key.secret"
 		with open (key_location) as keyfile:
 			api_key = keyfile.readline()
 			library = steam.get_library(test_id, api_key)
 		self.failIf(len(library) == 0)
+
+	def test_user_fromExactId(self):
+		user = User("icbat")
+		pass
+
+	def test_user_fromGarbage(self):
+		try:
+			user = User("-=-xcasdxoi213459sa")
+			self.assertTrue(False, "garbage input handed back a user class")
+		except LookupError:
+			pass
 
 if __name__ == '__main__':
 	unittest.main()
